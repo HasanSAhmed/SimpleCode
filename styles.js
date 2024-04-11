ba359f1811d5e423acc124efbbf42a459f82e5bf
HasanSAhmed <163813621+HasanSAhmed@users.noreply.github.com>
2024-04-11 03:00:14 -0400

    Update and rename style.js to styles.js

---
+++ styles.js
@@ -0,0 +1,8 @@
+  // Get button
+  const topBtn = document.getElementById("topbtn");
+  
+  // Scroll to top
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
